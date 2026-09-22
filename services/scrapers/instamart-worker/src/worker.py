020a725b49db3679bbc402582ca628915192fc6d
Divansh Singh <divanshsinghsaini123@gmail.com>
2026-09-22 18:44:52 -0400

    chore: install project dependencies

--- services/scrapers/instamart-worker/src/worker.py
+++ services/scrapers/instamart-worker/src/worker.py
@@ -1,5 +1,7 @@
 import logging
 
+
+# Extract podId (the dark store ID) and cartAllowedQuantity.quantityLimitBreachedMessage (which reveals if the store's stock is running critically low) into your platform_metadata JSONB column.
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("instamart-worker")
 
